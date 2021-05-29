3aaf24bc1cfe89530b41d5a6f83f9275877de939
Suhas More <suhas.more@joshsoftware.com>
2021-05-30 01:40:51 +0530

    implemented footer component

--- src/components/home/footer.jsx
+++ src/components/home/footer.jsx
@@ -1,6 +1,17 @@
+import './footer.css';
+
 const Footer = () => (
   <footer>
-    footer
+    <div className="footer-container">
+    Made by
+    <a href="https://suhas010.com" target="_blank" rel="noreferrer">Suhas010</a>
+    &#129299;
+    <div className="follow">
+      <a href="https://twitter.com/suhas0101" target="_blank" rel="noreferrer">Twitter</a>
+      <a href="https://linkedin.com/in/suhas-more" target="_blank" rel="noreferrer">LinkeIn</a>
+      <a href="https://github.com/suhas010" target="_blank" rel="noreferrer">GitHub</a>
+    </div>
+    </div>
   </footer>
 );
 
