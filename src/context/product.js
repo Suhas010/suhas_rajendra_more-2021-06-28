a17f508ff453e9dcf6c1a6f1dcd7678a2c05d1b3
Suhas More <suhas.more@joshsoftware.com>
2021-05-30 01:42:44 +0530

    added product context utils constants

---
+++ src/context/product.js
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import { ACTIONS, STATUS } from '../utils/constants'
+const ProductContext = React.createContext()
+
+const initialProductState = {
+  data: [],
+  selected: [],
+  status: STATUS.IDLE
+}
+
+function ProductProvider({ ...props}) {
+  const [state, dispatch] = React.useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case ACTIONS.PRODUCT_SUCCESS: {
+          return {
+            ...state,
+            status: STATUS.RESOLVED,
+            data: action.data,
+          }
+        }
+        case ACTIONS.PRODUCT_ERROR: {
+          return {
+            ...state,
+            data: [],
+            status: STATUS.REJECTED,
+            error: action.error
+          }
+        }
+        case ACTIONS.PRODUCT_FETCHING: {
+          return {
+            ...state,
+            status: STATUS.PENDING,
+          }
+        }
+      }
+    },
+    initialProductState,
+  )
+
+  const value = [state, dispatch]
+  return <ProductContext.Provider value={value} {...props} />
+}
+
+function useProduct() {
+  const context = React.useContext(ProductContext)
+  if (context === undefined) {
+    throw new Error(`useProduct must be used within a ProductProvider`)
+  }
+  return context
+}
+
+export {ProductProvider, useProduct}
