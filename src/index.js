211aaaea08dd3d1061064701f82317ac80972546
Suhas More <suhas.more@joshsoftware.com>
2021-05-30 18:53:25 +0530

    added theam provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/home';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from './context/teme';
 
 ReactDOM.render(
   <React.StrictMode>
+  <ThemeProvider>
     <Home />
+  </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
