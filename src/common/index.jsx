f99b8a5cd01f8726c70ecba50d354f90045d2c0f
Suhas More <suhas.more@joshsoftware.com>
2021-05-30 22:04:19 +0530

    renamed Error  component added index to common

---
+++ src/common/index.jsx
@@ -0,0 +1,5 @@
+export { default as  Button } from './Button';
+export { default as  Info } from './Info';
+export { default as  Loading } from './Loading';
+export { default as  SearchBox } from './SearchBox';
+export { default as  Image } from './Image';
