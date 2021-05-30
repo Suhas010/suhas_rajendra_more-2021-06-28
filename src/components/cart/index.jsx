f6b04e33acbb2bd2e569dec87bb5663195f9e9ce
Suhas More <suhas.more@joshsoftware.com>
2021-05-30 18:55:01 +0530

    updated app header component and added theam toggle

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,19 +1,22 @@
+import Info from "../../common/Error";
 import { useCart } from "../../context/cart"
 
 const Cart = () => {
   const [state, dispach] = useCart();
   const {data} = state;
   if(!data || !Object.keys(data).length) return (
-    <div className="section cart-container">
-      Empty Cart
-    </div>
+      <Info
+        title="Empty Cart"
+        type="info"
+        message="Seems like you have not added anything to cart yet.."
+      />
   )
   return (
     <div className="section">
       <div className="header">
         Header
       </div>
-      <div className=" cart-container">
+      <div className="cart-container">
         {Object.entries(data).map(([id,{name, count, price}]) => (
           <>
           <div key={id}>{name}</div>
--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -1,7 +1,33 @@
-const Header = () => (
-  <header>
-    Header
-  </header>
-);
+import { useContext } from "react";
+import Image from "../../common/Image";
+import moon from '../../assets/moon.svg';
+import sun from '../../assets/sun.svg';
+import ThemeContext from "../../context/teme";
+
+const Header = () => {
+  const { dark, toggle } = useContext(ThemeContext);
+
+  return (
+    <header className="app-header">
+      <h1>Vimeo</h1>
+      {dark && (
+        <Image
+          onClick={toggle}
+          src={sun}
+          alt="Sun Icon"
+          className="theam-icon"
+        />
+      )}
+      {!dark && (
+        <Image
+          onClick={toggle}
+          src={moon}
+          alt="Moon Icon"
+          className="theam-icon"
+        />
+      )}
+    </header>
+  );
+}
 
 export default Header
